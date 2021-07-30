67310f686300c61caff904921e75166458fe6f30
Elton Barbosa Gomes <eltonbarbosagomes@gmail.com>
2021-07-29 21:33:35 -0300

    add system.properties version java

--- src/main/java/manager/user/personApi/controller/PersonController.java
+++ src/main/java/manager/user/personApi/controller/PersonController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ap1/v1/person")
 public class PersonController {
-    
+
     @GetMapping
     public String getBook(){
         return "API test";
